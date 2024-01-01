7a53572b93fc1c44e29221f77b088fccdb66d861
EquipePosCimatec <154333975+EquipePosCimatec@users.noreply.github.com>
2024-01-01 17:31:43 -0300

    Buscador do Diário Oficial (MPBA).py

--- pages/Buscador do Diário Oficial (MPBA).py
+++ pages/Buscador do Diário Oficial (MPBA).py
@@ -81,7 +81,7 @@ def main():
         return
 
     except Exception as e:
-        st.error(f"Erro ao carregar o Diário Oficial: A data informada não teve publicação de Diário Oficial ou o Diário Oficial ainda não foi disponibilizado no servidor do MPBA. Se a data: {data_usuario} for dia útil, tente novamente mais tarde.")
+        st.error(f"Erro ao carregar o Diário Oficial: A data informada não teve publicação de Diário Oficial. Se o dia: {data_usuario} for a data de hoje, possivelmente o Diário Oficial ainda não foi disponibilizado no servidor do MPBA, neste caso, tente novamente mais tarde.")
         return
 
     # Solicitar o texto a ser buscado
